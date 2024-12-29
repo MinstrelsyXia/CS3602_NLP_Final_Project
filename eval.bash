405478ec5f45cebae7c024f02734d782e2ca0937
char_lottexxy <char-lotte@sjtu.edu.cn>
2024-12-29 14:28:59 +0800

    finished nlp-hw1

--- eval.bash
+++ eval.bash
@@ -1,4 +1,4 @@
-hf_path="output/1430/checkpoint-40000"
+hf_path="output/0.5B/checkpoint-50000"
 work_dir="/ssd/xiaxinyuan/code/CS3602_NLP_Final_Project/evals/plm"
 opencompass \
     --datasets mmlu_ppl hellaswag_clean_ppl winogrande_ll ARC_e_ppl ARC_c_clean_ppl SuperGLUE_BoolQ_few_shot_ppl \
